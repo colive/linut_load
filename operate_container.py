630fa7c654c3efc5e64f38b591f7ae64542b223e
colive <colive@users.noreply.github.com>
2017-04-16 22:13:27 +0800

    Create operate_container.py
    add operate container

---
+++ operate_container.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+#coding='gbk'
+
+#author:colive
+#use for:add or del dockers ,and when the docker or machine is 
+#wrong ,can move the docker auto
+
+def add_docker(location,server):
+    #find the machine and pull docker images
+def del_docker(location,server):
+
+def move_docker(docker_name): 
+
+def modify_quota(server,type='cpu',num):
+
+def move_machine(inner_ip):
+
+
+
